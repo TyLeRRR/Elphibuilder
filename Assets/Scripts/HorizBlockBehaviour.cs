168205ef8df811268bbaff8f81a60672da44c4ec
Taras <sologub.taras@gmail.com>
2017-10-29 15:25:17 +0100

    Last commit before tutorial

--- Assets/Scripts/HorizBlockBehaviour.cs
+++ Assets/Scripts/HorizBlockBehaviour.cs
@@ -6,8 +6,7 @@ public class HorizBlockBehaviour : MonoBehaviour
 {
     private float timeLeftToStart = 1.5f;
     private float nextStepTimestamp;
-
-//    private GameObject blockHorizontal;
+    public bool isCollided;
 
     void Start()
     {
@@ -15,20 +14,31 @@ public class HorizBlockBehaviour : MonoBehaviour
         nextStepTimestamp = Time.realtimeSinceStartup + 0.5f;
     }
 
-    void FixedUpdate()
+    void Update()
     {
         timeLeftToStart -= Time.deltaTime;
-
 
         if (timeLeftToStart < 0)
         {
             gameObject.GetComponent<Renderer>().enabled = true;
+
             //move the block every 0.5sec on 0.5 down
             if (nextStepTimestamp <= Time.realtimeSinceStartup)
             {
                 transform.Translate(new Vector3(0, -0.5f, 0), Space.World);
                 nextStepTimestamp = Time.realtimeSinceStartup + 0.5f;
+                
+                if(IsCollided)
+                {
+                    transform.Translate(0,0,0);
+                }
             }
         }
+    }
+
+    public bool IsCollided
+    {
+        get { return isCollided; }
+        set { isCollided = value; }
     }
 }
