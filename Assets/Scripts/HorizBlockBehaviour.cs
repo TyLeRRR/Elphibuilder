6c10b94dfeed6bf674034ff186a395509b7eedfc
Taras <sologub.taras@gmail.com>
2017-10-29 09:58:34 +0100

    Movement down horizBlock

--- Assets/Scripts/HorizBlockBehaviour.cs
+++ Assets/Scripts/HorizBlockBehaviour.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HorizBlockBehaviour : MonoBehaviour {
+public class HorizBlockBehaviour : MonoBehaviour
+{
+    private float timeLeftToStart = 1.5f;
+    private float nextStepTimestamp;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+//    private GameObject blockHorizontal;
+
+    void Start()
+    {
+        gameObject.GetComponent<Renderer>().enabled = false;
+        nextStepTimestamp = Time.realtimeSinceStartup + 0.5f;
+    }
+
+    void FixedUpdate()
+    {
+        timeLeftToStart -= Time.deltaTime;
+
+
+        if (timeLeftToStart < 0)
+        {
+            gameObject.GetComponent<Renderer>().enabled = true;
+            //move the block every 0.5sec on 0.5 down
+            if (nextStepTimestamp <= Time.realtimeSinceStartup)
+            {
+                transform.Translate(new Vector3(0, -0.5f, 0), Space.World);
+                nextStepTimestamp = Time.realtimeSinceStartup + 0.5f;
+            }
+        }
+    }
 }
